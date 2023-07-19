96e392088e52fa71829f862c9fdf5d005faf40ca
Keiran <teacherkeiran@gmail.com>
2023-07-19 16:26:32 +0100

    Set up dynamic rendering of blog-single. Delete comments section from blog-single.

--- main.py
+++ main.py
@@ -1,21 +1,59 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import requests
+import smtplib
+from pprint import pprint
 
-app = Flask(__name__)
+#_____________________________________________Constants______________________________________________________#
 
 BLOGS_API = 'https://api.npoint.io/802720ad4cf9deb722b0'
-
 response = requests.get(BLOGS_API)
 blog_posts = response.json()
 
+MY_EMAIL = 'keirandevinetest@gmail.com'
+MY_PASSWORD = 'xqtkxyhvmfisjqfi'
+
+app = Flask(__name__)
+
+
+#_________________________________________Definition of Functions____________________________________________#
+
+def send_email(name, email, subject, message):
+    email_message = f"Subject: New Message\n\nName: {name}\nEmail: {email}\nSubject: {subject}\nMessage: {message}"
+    with smtplib.SMTP('smtp.gmail.com') as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL, to_addrs=MY_EMAIL, msg=email_message)
+        connection.close()
+
+
+#______________________________________Flask Server Routes___________________________________________________#
 @app.route('/')
 def home():
+    pprint(blog_posts)
     return render_template('index.html', blogs=blog_posts)
 
 
-@app.route('/blog')
-def get_post():
-    return render_template('blog-single.html', blogs=blog_posts)
+@app.route('/blog/<int:blog_id>')
+def get_post(blog_id):
+    selected_post = {}
+    for post in blog_posts:
+        if post['id'] == blog_id:
+            selected_post = post
+            pprint(selected_post)
+    return render_template('blog-single.html', post=selected_post, encodings='utf-8')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def get_contact_info():
+    name = request.form['name']
+    email = request.form['email']
+    subject = request.form['subject']
+    message = request.form['message']
+    print(name, email, subject, message)
+    send_email(name, email, subject, message)
+    return render_template('index.html')
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
